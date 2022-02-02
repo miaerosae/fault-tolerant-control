a6550c8633a916eb9cdab389caf1d0df9815545b
Miae Kim <80808711+miaerosae@users.noreply.github.com>
2022-02-02 16:13:14 +0900

    control안됨... 이유는 모르겠지만 갑자기 발산

---
+++ test/feedbacklinearization_miae.py
@@ -0,0 +1,116 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from fym.core import BaseEnv, BaseSystem
+import fym.logging
+from fym.utils.rot import angle2quat, quat2angle
+
+import ftc.config
+from ftc.models.multicopter import Multicopter
+from ftc.agents.CA import CA
+import ftc.agents.fl_miae as fl
+from ftc.plotting import exp_plot
+from copy import deepcopy
+from ftc.faults.actuator import LoE
+from ftc.faults.manager import LoEManager
+cfg = ftc.config.load()
+
+
+class Env(BaseEnv):
+    def __init__(self):
+        super().__init__(dt=0.1, max_t=10)
+        init = cfg.models.multicopter.init
+        self.plant = Multicopter(init.pos, init.vel, init.quat, init.omega)
+        n = self.plant.mixer.B.shape[1]
+
+        # Define actuator dynamics
+        # self.act_dyn = ActuatorDynamcs(tau=0.01, shape=(n, 1))
+
+        # Define faults
+        self.sensor_faults = []
+        self.fault_manager = LoEManager([
+            # LoE(time=3, index=0, level=0.),  # scenario a
+            # LoE(time=6, index=2, level=0.),  # scenario b
+        ], no_act=n)
+
+        # Define FDI
+        self.fdi = self.fault_manager.fdi
+
+        # Define agents
+        self.CA = CA(self.plant.mixer.B)
+        self.controller = fl.FLController(self.plant.m,
+                                          self.plant.g,
+                                          self.plant.J)
+
+        self.detection_time = self.fault_manager.fault_times + self.fdi.delay
+
+        # Set references
+        pos_des = np.vstack([-1, 1, 2])
+        vel_des = np.vstack([0, 0, 0])
+        quat_des = np.vstack([1, 0, 0, 0])
+        omega_des = np.vstack([0, 0, 0])
+        self.ref = np.vstack([pos_des, vel_des, quat_des, omega_des])
+
+    def step(self):
+        *_, done = self.update()
+        return done
+
+    def get_ref(self, t):
+        return self.ref
+
+    def set_dot(self, t):
+        W = self.fdi.get_true(t)
+        What = self.fdi.get(t)
+        ref = self.get_ref(t)
+
+        # Controller
+        virtual_ctrl = self.controller.get_virtual(t,
+                                                   self.plant,
+                                                   ref)
+        forces = self.controller.get_FM(virtual_ctrl)
+        rotors_cmd = self.CA.get(What).dot(forces)
+
+        # actuator saturation
+        rotors = np.clip(rotors_cmd, 0, self.plant.rotor_max)
+
+        # Set actuator faults
+        rotors = self.fault_manager.get_faulty_input(t, rotors)
+
+        self.plant.set_dot(t, rotors)
+        self.controller.set_dot(virtual_ctrl)
+
+        return dict(t=t, x=self.plant.observe_dict(), What=What,
+                    rotors=rotors, rotors_cmd=rotors_cmd, W=W, ref=ref)
+
+
+def run(loggerpath):
+    env = Env()
+    env.logger = fym.Logger(loggerpath)
+    env.logger.set_info(cfg=ftc.config.load())
+
+    env.reset()
+
+    while True:
+        env.render()
+        done = env.step()
+
+        if done:
+            env_info = {
+                "detection_time": env.detection_time,
+                "rotor_min": env.plant.rotor_min,
+                "rotor_max": env.plant.rotor_max,
+            }
+            env.logger.set_info(**env_info)
+            break
+
+    env.close()
+
+
+def exp1(loggerpath):
+    run(loggerpath)
+
+
+if __name__ == "__main__":
+    loggerpath = "data.h5"
+    exp1(loggerpath)
+    exp_plot(loggerpath)
