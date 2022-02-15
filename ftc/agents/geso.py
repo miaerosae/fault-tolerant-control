c124e85f1b7fbcffc31bc84ac875994828041834
Miae Kim <80808711+miaerosae@users.noreply.github.com>
2022-02-15 17:22:23 +0900

    geso, lpeso observer

---
+++ ftc/agents/geso.py
@@ -0,0 +1,94 @@
+from fym.core import BaseEnv, BaseSystem
+
+import numpy as np
+
+
+class GESO_pos(BaseEnv):
+    '''
+    basic generalized extended state observer (for position tracking)
+
+    *Set disturbance poly as d = p0 + p1t (n=2)
+
+    Parameters:
+        L: observer gain matrix (6 by 1)
+    '''
+    def __init__(self, L):
+        super().__init__()
+        self.xi = BaseSystem(np.zeros((6, 1)))
+
+        self.A = np.array([[0, 1, 0, 0, 0, 0],
+                           [0, 0, 1, 0, 0, 0],
+                           [0, 0, 0, 1, 0, 0],
+                           [0, 0, 0, 0, 1, 0],
+                           [0, 0, 0, 0, 0, 1],
+                           [0, 0, 0, 0, 0, 0]])
+        self.B = np.array([0, 0, 0, 1, 0, 0])[:, None]
+        self.C = np.array([[1, 0, 0, 0, 0, 0]])
+        self.L = L
+
+    def deriv(self, xi, y, v):
+        L, A, B, C = self.L, self.A, self.B, self.C
+
+        y1 = C.dot(self.xi.state)
+        xidot = A.dot(xi) + B.dot(v) + L.dot(y - y1)
+        return xidot
+
+    def get_dist_obs(self, y):
+        y1 = self.C.dot(self.xi.state)
+        disturbance = y - y1
+        observation = y1
+        return disturbance, observation
+
+    def set_dot(self, t, y, v):
+        '''
+        y: desired observer's real state
+        v: input made from controller
+        '''
+        states = self.observe_list()
+        dots = self.deriv(*states, y, v)
+        self.xi.dot = dots
+
+
+class GESO_psi(BaseEnv):
+    '''
+    L: (4 by 1)
+    '''
+    def __init__(self, L):
+        super().__init__()
+        self.xi = BaseSystem(np.zeros((4, 1)))
+
+        self.A = np.array([[0, 1, 0, 0],
+                           [0, 0, 1, 0],
+                           [0, 0, 0, 1],
+                           [0, 0, 0, 0]])
+        self.B = np.array([0, 1, 0, 0])[:, None]
+        self.C = np.array([[1, 0, 0, 0]])
+        self.L = L
+
+    def deriv(self, xi, y, v):
+        L, A, B, C = self.L, self.A, self.B, self.C
+
+        y1 = C.dot(self.xi.state)
+        xidot = A.dot(xi) + B.dot(v) + L.dot(y - y1)
+        return xidot
+
+    def get_dist_obs(self, y):
+        y1 = self.C.dot(self.xi.state)
+        disturbance = y - y1
+        observation = y1
+        return disturbance, observation
+
+    def set_dot(self, t, y, v):
+        '''
+        y: desired observer's real state
+        v: input made from controller
+        '''
+        states = self.observe_list()
+        dots = self.deriv(*states, y, v)
+        self.xi.dot = dots
+
+
+if __name__ == "__main__":
+    system = GESO_psi(np.vstack([1, 1, 1, 1]))
+    system.set_dot(t=0, y=np.zeros((1, 1)), v=np.zeros((1, 1)))
+    print(repr(system))
--- ftc/agents/param.py
+++ ftc/agents/param.py
@@ -5,5 +5,5 @@ import numpy as np
 
 
 def get_b0(m, g, J):
-    b0 = np.vstack((-g/J[1, 1], g/J[0, 0], -1/m, 1/J[2, 2]))
+    b0 = np.array([-g/J[1, 1], g/J[0, 0], -1/m, 1/J[2, 2]])
     return b0
