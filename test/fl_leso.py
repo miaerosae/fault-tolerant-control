9aa4fd37bde280c7f7620cd69b3b03bee8440d7f
Miae Kim <80808711+miaerosae@users.noreply.github.com>
2022-03-25 12:18:52 +0900

    leso and geso, not include fl model

--- test/fl_leso.py
+++ test/fl_leso.py
@@ -9,7 +9,7 @@ from fym.utils.rot import angle2quat, quat2angle
 import ftc.config
 from ftc.models.multicopter import Multicopter
 from ftc.agents.CA import CA
-import ftc.agents.lpeso as lpeso
+import ftc.agents.leso as leso
 from ftc.agents.param import get_b0
 from ftc.plotting import exp_plot
 from copy import deepcopy
@@ -26,7 +26,7 @@ cfg = ftc.config.load()
 
 class Env(BaseEnv):
     def __init__(self):
-        super().__init__(dt=0.05, max_t=40)
+        super().__init__(dt=0.05, max_t=30)
         init_pos = np.vstack((0, 0, 0))
         # init_ang = np.deg2rad([20, 30, 10])*(np.random.rand(3) - 0.5)
         # init_quat = (angle2quat(init_ang[2], init_ang[1], init_ang[0]))
@@ -46,8 +46,8 @@ class Env(BaseEnv):
         # Define faults
         self.sensor_faults = []
         self.fault_manager = LoEManager([
-            LoE(time=3, index=0, level=0.5),  # scenario a
-            LoE(time=6, index=2, level=0.8),  # scenario b
+            LoE(time=3, index=0, level=0.8),  # scenario a
+            # LoE(time=6, index=2, level=0.8),  # scenario b
         ], no_act=n)
 
         # Define FDI
@@ -55,26 +55,25 @@ class Env(BaseEnv):
 
         # Define agents
         self.CA = CA(self.plant.mixer.B)
-        self.controller = lpeso.Controller(self.plant.m,
-                                           self.plant.g)
+        self.controller = leso.Controller(self.plant.m,
+                                          self.plant.g)
         b0 = get_b0(self.plant.m, self.plant.g, self.plant.J)
-        self.b0 = b0
         K = np.array([[23, 484],
                       [23, 183.1653],
                       [23, 80.3686],
                       [23, 30.0826]])
         Lx, Ly, Lz, Lpsi = 1., 1., 55., 1.
-        self.lpeso_x = lpeso.lowPowerESO(4, 2, K, b0[0], self.controller.F[0, :], Lx)
-        self.lpeso_y = lpeso.lowPowerESO(4, 2, K, b0[1], self.controller.F[1, :], Ly)
-        self.lpeso_z = lpeso.lowPowerESO(4, 2, K, b0[2], self.controller.F[2, :], Lz)
+        self.leso_x = leso.lowPowerESO(4, 2, K, b0[0], self.controller.F[0, :], Lx)
+        self.leso_y = leso.lowPowerESO(4, 2, K, b0[1], self.controller.F[1, :], Ly)
+        self.leso_z = leso.lowPowerESO(4, 2, K, b0[2], self.controller.F[2, :], Lz)
         H = np.array([[3, 3, 1]])
         # H = np.array([[30, 291, 910]])
-        self.hgeso_psi = lpeso.highGainESO(0.5, H, b0[3], self.controller.F[3, 0:2], Lpsi)
+        self.hgeso_psi = leso.highGainESO(0.5, H, b0[3], self.controller.F[3, 0:2], Lpsi)
 
         self.detection_time = self.fault_manager.fault_times + self.fdi.delay
 
         # Set references
-        pos_des = np.vstack([-2, 5, 4])
+        pos_des = np.vstack([-2, 3, 4])
         vel_des = np.vstack([0, 0, 0])
         quat_des = np.vstack([1, 0, 0, 0])
         omega_des = np.vstack([0, 0, 0])
@@ -106,23 +105,23 @@ class Env(BaseEnv):
         # Observer
         obs_ctrl = np.zeros((4, 1))
         obs_ref = self.get_obs_ref()
-        obs_ctrl[2] = self.lpeso_x.get_virtual(t, obs_ref[0, :, :])
-        obs_ctrl[1] = self.lpeso_y.get_virtual(t, obs_ref[1, :, :])
-        obs_ctrl[0] = self.lpeso_z.get_virtual(t, obs_ref[2, :, :])
+        obs_ctrl[2] = self.leso_x.get_virtual(t, obs_ref[0, :, :])
+        obs_ctrl[1] = self.leso_y.get_virtual(t, obs_ref[1, :, :])
+        obs_ctrl[0] = self.leso_z.get_virtual(t, obs_ref[2, :, :])
         obs_ctrl[3] = self.hgeso_psi.get_virtual(obs_ref[3, 0:2, :])
 
         observation = np.zeros((4, 1))
-        observation[0] = self.lpeso_x.get_obs()
-        observation[1] = self.lpeso_y.get_obs()
-        observation[2] = self.lpeso_z.get_obs()
+        observation[0] = self.leso_x.get_obs()
+        observation[1] = self.leso_y.get_obs()
+        observation[2] = self.leso_z.get_obs()
         observation[3] = self.hgeso_psi.get_obs()
 
         # Controller
         virtual_ctrl = self.controller.get_virtual(t, obs_ctrl)
 
         forces = self.controller.get_FM(virtual_ctrl)
-        rotors_cmd = self.CA.get(What).dot(forces)
-        # rotors_cmd = np.linalg.pinv(self.plant.mixer.B).dot(forces)
+        # rotors_cmd = self.CA.get(What).dot(forces)
+        rotors_cmd = np.linalg.pinv(self.plant.mixer.B).dot(forces)
 
         # actuator saturation
         rotors = np.clip(rotors_cmd, 0, self.plant.rotor_max)
@@ -134,9 +133,9 @@ class Env(BaseEnv):
         self.controller.set_dot(virtual_ctrl)
 
         y = np.vstack([self.plant.pos.state, quat2angle(self.plant.quat.state)[::-1][2]])
-        self.lpeso_x.set_dot(t, y[0], obs_ref[0, :, :])
-        self.lpeso_y.set_dot(t, y[1], obs_ref[1, :, :])
-        self.lpeso_z.set_dot(t, y[2], obs_ref[2, :, :])
+        self.leso_x.set_dot(t, y[0], obs_ref[0, :, :])
+        self.leso_y.set_dot(t, y[1], obs_ref[1, :, :])
+        self.leso_z.set_dot(t, y[2], obs_ref[2, :, :])
         self.hgeso_psi.set_dot(t, y[3], obs_ref[3, 0:2, :])
 
         return dict(t=t, x=self.plant.observe_dict(), What=What,
