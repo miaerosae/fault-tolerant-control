dda81534826b03e5ab6b7cfc2fa3ac92743091f5
MiaeKim <miaerosae@gmail.com>
2022-07-28 17:21:51 +0900

    add adaptive method

---
+++ ftc/agents/ABLF.py
@@ -0,0 +1,121 @@
+from fym.core import BaseEnv, BaseSystem
+
+import numpy as np
+
+
+class outerLoop(BaseEnv):
+    def __init__(self, alp, eps, K, rho, k, init):
+        super().__init__()
+        self.e = BaseSystem(np.vstack([init, 0, 0]))
+
+        self.alp, self.eps, self.K, self.k = alp, eps, K, k
+        self.rho_0, self.rho_inf = rho.ravel()
+
+    def deriv(self, e, y, ref, t):
+        alp, eps = self.alp, self.eps
+        e_real = y - ref
+
+        q = self.get_virtual(t)
+        edot = np.zeros((3, 1))
+        edot[0, :] = e[1] + (alp[0]/eps) * (e_real - e[0])
+        edot[1, :] = e[2] + q + (alp[1]/eps**2) * (e_real - e[0])
+        edot[2, :] = (alp[2]/eps**3) * (e_real - e[0])
+        return edot
+
+    def get_virtual(self, t):
+        rho_0, rho_inf, k, K = self.rho_0, self.rho_inf, self.k, self.K
+        e = self.e.state
+        rho = (rho_0-rho_inf) * np.exp(-k*t) + rho_inf
+        drho = - k * (rho_0-rho_inf) * np.exp(-k*t)
+        ddrho = k**2 * (rho_0-rho_inf) * np.exp(-k*t)
+
+        z1 = e[0] / rho
+        dz1 = e[1]/rho - e[0]*drho/rho**2
+        alpha = - (1-z1**2)*rho*K[0]*z1 + drho*z1
+        z2 = e[1] - alpha
+        dalpha = 2*rho*K[0]*dz1*z1**2 - (1-z1**2)*drho*K[0]*z1 \
+            - (1-z1**2)*rho*K[0]*dz1 + ddrho*z1 + drho*dz1
+        q = - e[2] + dalpha - K[1]*z2 - z1/(1-z1**2)/rho
+        return q
+
+    def set_dot(self, t, y, ref):
+        states = self.observe_list()
+        self.e.dot = self.deriv(*states, y, ref, t)
+
+    def get_err(self):
+        return self.e.state[0]
+
+    def get_dist(self):
+        return self.e.state[2]
+
+
+class innerLoop(BaseEnv):
+    '''
+    xi: lower and upper bound of u (moments for my case), [lower, upper]
+    rho: bound of state x, dx
+    virtual input nu = f + b*u
+    '''
+    def __init__(self, alp, eps, K, xi, rho, c, b, g):
+        super().__init__()
+        self.x = BaseSystem(np.zeros((3, 1)))
+        self.lamb = BaseSystem(np.zeros((2, 1)))
+        self.kappa = BaseSystem((np.ones((1, 1))))
+
+        self.alp, self.eps, self.K = alp, eps, K
+        self.xi, self.rho = xi, rho
+        self.c, self.b, self.g = c, b, g
+
+    def deriv(self, x, lamb, kappa, t, y1, y2, ref, f):
+        alp, eps, b = self.alp, self.eps, self.b
+        u = self.get_virtual(t, ref, f)
+        u_sat = np.clip(u, self.xi[0], self.xi[1])
+        xdot = np.zeros((3, 1))
+        xdot[0, :] = x[1] + (alp[0]/eps) * (y1 - x[0])
+        xdot[1, :] = x[2] + kappa*f + u_sat/b + (alp[1]/eps**2) * (y1 - x[0])
+        xdot[2, :] = (alp[2]/eps**3) * (y1 - x[0])
+        lambdot = np.zeros((2, 1))
+        lambdot[0] = - self.c[0]*lamb[0] + lamb[1]
+        lambdot[1] = - self.c[1]*lamb[1] + (u_sat - u)
+        kappadot = (y2-x[1])*f
+        return xdot, lambdot, kappadot
+
+    def get_virtual(self, t, ref, f):
+        K, c, rho = self.K, self.c, self.rho
+        x = self.x.state
+        # lamb = self.lamb.state
+        lamb = np.zeros((2, 1))
+        kappa = self.kappa.state
+        dref, ddref = 0, 0
+
+        z1 = x[0] - ref - lamb[0]
+        alpha = - K[0]*z1 - c[0]*lamb[0]
+        z2 = x[1] - dref - alpha - lamb[1]
+        dalpha = - K[0]*(x[1] - dref + c[0]*lamb[0] - lamb[1]) \
+            - c[0]*(-c[0]*lamb[0] + lamb[1])
+        u = self.b * (- c[1]*lamb[1] + dalpha + ddref - K[1]*z2
+                      - (rho[1]**2 - z2**2)/(rho[0]**2 - z1**2)*z1 - x[2]
+                      - kappa*f)
+        return u
+
+    def get_u(self, t, ref, f):
+        u = self.get_virtual(t, ref, f)
+        u_sat = np.clip(u, self.xi[0], self.xi[1])
+        return u_sat
+
+    def set_dot(self, t, y1, y2, ref, f):
+        states = self.observe_list()
+        dots = self.deriv(*states, t, y1, y2, ref, f)
+        self.x.dot, self.lamb.dot, self.kappa.dot = dots
+
+    def get_obs(self):
+        return self.x.state[0]
+
+    def get_obsdot(self):
+        return self.x.state[1]
+
+    def get_dist(self):
+        return self.x.state[2]
+
+
+if __name__ == "__main__":
+    pass
---
+++ test/simul_ABLF.py
@@ -0,0 +1,288 @@
+import ray
+from ray import tune
+import argparse
+import numpy as np
+from numpy import sin, cos
+import matplotlib.pyplot as plt
+
+from fym.core import BaseEnv, BaseSystem
+import fym.logging
+from fym.utils.rot import angle2quat, quat2angle
+
+import ftc.config
+from ftc.models.multicopter import Multicopter
+from ftc.agents.CA import CA
+import ftc.agents.ABLF as ABLF
+from ftc.agents.param import get_b0
+from ftc.plotting import exp_plot
+from copy import deepcopy
+from ftc.faults.actuator import LoE
+from ftc.faults.manager import LoEManager
+
+plt.rc("text", usetex=False)
+plt.rc("lines", linewidth=1)
+plt.rc("axes", grid=True)
+plt.rc("grid", linestyle="--", alpha=0.8)
+
+cfg = ftc.config.load()
+
+
+class Env(BaseEnv):
+    def __init__(self):
+        super().__init__(dt=0.01, max_t=30)
+        init = cfg.models.multicopter.init
+        self.plant = Multicopter(init.pos, init.vel, init.quat, init.omega,
+                                 uncertainty=True,
+                                 )
+        self.n = self.plant.mixer.B.shape[1]
+
+        # Define actuator dynamics
+        # self.act_dyn = ActuatorDynamcs(tau=0.01, shape=(n, 1))
+
+        # Define faults
+        self.sensor_faults = []
+        self.fault_manager = LoEManager([
+            # LoE(time=3, index=0, level=0.5),  # scenario a
+            # LoE(time=6, index=2, level=0.8),  # scenario b
+        ], no_act=self.n)
+
+        # Define FDI
+        self.fdi = self.fault_manager.fdi
+
+        # Define agents
+        self.CA = CA(self.plant.mixer.B)
+        params = cfg.agents.BLF
+        k11, k12, k21, k22, k31, k32, k41, k42 = cfg.agents.BLF.K.ravel()
+        Kx = np.array([k11, k12])
+        Ky = np.array([k21, k22])
+        Kz = np.array([k31, k32])
+        self.pos_ref = np.vstack([-0, 1/2, 0])
+        self.blf_x = ABLF.outerLoop(params.oL.alp, params.oL.eps[0], Kx,
+                                    params.oL.rho, params.oL.rho_k,
+                                    -self.pos_ref[0][0])
+        self.blf_y = ABLF.outerLoop(params.oL.alp, params.oL.eps[1], Ky,
+                                    params.oL.rho, params.oL.rho_k,
+                                    -self.pos_ref[1][0])
+        self.blf_z = ABLF.outerLoop(params.oL.alp, params.oL.eps[2], Kz,
+                                    params.oL.rho, params.oL.rho_k,
+                                    -self.pos_ref[2][0])
+        J = np.diag(self.plant.J)
+        b = np.array([1/J[0], 1/J[1], 1/J[2]])
+        # Kang = np.array([20, 15])  # for rotor failure case
+        Kang = np.array([k41, k42])
+        self.blf_phi = ABLF.innerLoop(params.iL.alp, params.iL.eps[0], Kang,
+                                      params.iL.xi, params.iL.rho,
+                                      params.iL.c, b[0], self.plant.g)
+        self.blf_theta = ABLF.innerLoop(params.iL.alp, params.iL.eps[1], Kang,
+                                        params.iL.xi, params.iL.rho,
+                                        params.iL.c, b[1], self.plant.g)
+        self.blf_psi = ABLF.innerLoop(params.iL.alp, params.iL.eps[2], Kang,
+                                      params.iL.xi, params.iL.rho,
+                                      params.iL.c, b[2], self.plant.g)
+
+        self.detection_time = self.fault_manager.fault_times + self.fdi.delay
+        self.rotors_cmd = np.zeros((6, 1))
+
+    def get_ref(self, t):
+        # pos_des = self.pos_ref
+        pos_des = np.vstack([np.sin(t)/2, np.cos(t)/2, -t])
+        vel_des = np.vstack([0, 0, 0])
+        # pi = np.pi
+        # pos_des = np.vstack([np.sin(5*pi*t/10)*np.cos(pi*t/10)*cos(pi/4),
+        #                      np.sin(5*pi*t/10)*np.sin(pi*t/10)*cos(pi/4),
+        #                      sin(5*pi*t/10)*sin(pi/4)])
+        quat_des = np.vstack([1, 0, 0, 0])
+        omega_des = np.vstack([0, 0, 0])
+        ref = np.vstack([pos_des, vel_des, quat_des, omega_des])
+        return ref
+
+    def step(self):
+        *_, done = self.update()
+
+        # Stop condition
+        # omega = self.plant.omega.state
+        # for dang in omega:
+        #     if abs(dang) > np.deg2rad(80):
+        #         done = True
+        # err_pos = np.array([self.blf_x.e.state[0],
+        #                     self.blf_y.e.state[0],
+        #                     self.blf_z.e.state[0]])
+        # for err in err_pos:
+        #     if abs(err) > 10:
+        #         done = True
+
+        # for rotor in self.rotors_cmd:
+        #     if rotor < 0 or rotor > self.plant.rotor_max + 5:
+        #         done = True
+
+        return done
+
+    def get_windvel(self, t):
+        pi = np.pi
+        return 0.5*np.sin(pi*t+1)
+
+    def set_dot(self, t):
+        ref = self.get_ref(t)
+        W = self.fdi.get_true(t)
+        What = self.fdi.get(t)
+        # windvel = self.get_windvel(t)
+
+        # Outer-Loop: virtual input
+        q = np.zeros((3, 1))
+        q[0] = self.blf_x.get_virtual(t)
+        q[1] = self.blf_y.get_virtual(t)
+        q[2] = self.blf_z.get_virtual(t)
+        # q[0] = np.clip(self.blf_x.get_virtual(t),
+        #                - self.plant.rotor_max*2/self.plant.m/2,
+        #                self.plant.rotor_max*2/self.plant.m/2)
+        # q[1] = np.clip(self.blf_y.get_virtual(t),
+        #                - self.plant.rotor_max*3/self.plant.m/2,
+        #                self.plant.rotor_max*3/self.plant.m/2)
+        # q[2] = np.clip(self.blf_z.get_virtual(t)-self.plant.g,
+        #                - self.plant.rotor_max*self.n/self.plant.m,
+        #                0)
+
+        # Inverse solution
+        u1_cmd = self.plant.m * (q[0]**2 + q[1]**2 + (q[2]-self.plant.g)**2)**(1/2)
+        phid = np.clip(np.arcsin(q[1] * self.plant.m / u1_cmd),
+                       - np.deg2rad(40), np.deg2rad(40))
+        thetad = np.clip(np.arctan(q[0] / (q[2] - self.plant.g)),
+                         - np.deg2rad(40), np.deg2rad(40))
+        psid = 0
+        eulerd = np.vstack([phid, thetad, psid])
+
+        # caculate f
+        J = np.diag(self.plant.J)
+        obs_p = self.blf_phi.get_obsdot()
+        obs_q = self.blf_theta.get_obsdot()
+        obs_r = self.blf_psi.get_obsdot()
+        f = np.array([(J[1]-J[2]) / J[0] * obs_q * obs_r,
+                      (J[2]-J[0]) / J[1] * obs_p * obs_r,
+                      (J[0]-J[1]) / J[2] * obs_p * obs_q])
+
+        # Inner-Loop
+        u2 = self.blf_phi.get_u(t, phid, f[0])
+        u3 = self.blf_theta.get_u(t, thetad, f[1])
+        u4 = self.blf_psi.get_u(t, psid, f[2])
+
+        # Saturation u1
+        u1 = np.clip(u1_cmd, 0, self.plant.rotor_max*self.n)
+
+        # rotors
+        forces = np.vstack([u1, u2, u3, u4])
+        # rotors_cmd = np.linalg.pinv(self.plant.mixer.B).dot(forces)
+        rotors_cmd = self.CA.get(What).dot(forces)
+        rotors = np.clip(rotors_cmd, 0, self.plant.rotor_max)
+        self.rotors_cmd = rotors_cmd
+
+        # Set actuator faults
+        rotors = self.fault_manager.get_faulty_input(t, rotors)
+
+        # Disturbance
+        dist = np.zeros((6, 1))
+        dist[0] = self.blf_x.get_dist()
+        dist[1] = self.blf_y.get_dist()
+        dist[2] = self.blf_z.get_dist()
+        dist[3] = self.blf_phi.get_dist()
+        dist[4] = self.blf_theta.get_dist()
+        dist[5] = self.blf_psi.get_dist()
+
+        # Observation
+        obs_pos = np.zeros((3, 1))
+        obs_pos[0] = self.blf_x.get_err()
+        obs_pos[1] = self.blf_y.get_err()
+        obs_pos[2] = self.blf_z.get_err()
+        obs_ang = np.zeros((3, 1))
+        obs_ang[0] = self.blf_phi.get_obs()
+        obs_ang[1] = self.blf_theta.get_obs()
+        obs_ang[2] = self.blf_psi.get_obs()
+
+        # set_dot
+        self.plant.set_dot(t, rotors,
+                           # windvel
+                           )
+        x, y, z = self.plant.pos.state.ravel()
+        euler = quat2angle(self.plant.quat.state)[::-1]
+        omega = self.plant.omega.state
+        self.blf_x.set_dot(t, x, ref[0])
+        self.blf_y.set_dot(t, y, ref[1])
+        self.blf_z.set_dot(t, z, ref[2])
+        self.blf_phi.set_dot(t, euler[0], omega[0], phid, f[0])
+        self.blf_theta.set_dot(t, euler[1], omega[1], thetad, f[1])
+        self.blf_psi.set_dot(t, euler[2], omega[2], psid, f[2])
+
+        return dict(t=t, x=self.plant.observe_dict(), What=What,
+                    rotors=rotors, rotors_cmd=rotors_cmd, W=W, ref=ref,
+                    virtual_u=forces, dist=dist, q=q,
+                    obs_pos=obs_pos, obs_ang=obs_ang, eulerd=eulerd)
+
+
+def run_ray(k11, k12, k21, k22, k31, k32):
+    env = Env(k11, k12, k21, k22, k31, k32)
+    env.reset()
+
+    while True:
+        env.render()
+        done = env.step()
+
+        if done:
+            break
+
+    time = env.clock.get()
+    env.close()
+
+    return time
+
+
+def run(loggerpath):
+    env = Env()
+    env.logger = fym.Logger(loggerpath)
+    env.logger.set_info(cfg=ftc.config.load())
+
+    env.reset()
+
+    while True:
+        env.render()
+        done = env.step()
+
+        if done:
+            env_info = {
+                "detection_time": env.detection_time,
+                "rotor_min": env.plant.rotor_min,
+                "rotor_max": env.plant.rotor_max,
+            }
+            env.logger.set_info(**env_info)
+            break
+
+    env.close()
+
+
+def main(args):
+    if args.with_ray:
+        configs = {
+            "k11": tune.uniform(1, 10),
+            "k12": tune.uniform(1, 10),
+            "k21": tune.uniform(1, 20),
+            "k22": tune.uniform(1, 20),
+            "k31": tune.uniform(1, 20),
+            "k32": tune.uniform(1, 20),
+        }
+
+        def trainable(configs):
+            score = run_ray(configs["k11"], configs["k12"], configs["k21"],
+                            configs["k22"], configs["k31"], configs["k32"])
+            tune.report(score=score)
+        tune.run(trainable, config=configs, num_samples=10)
+
+    else:
+        loggerpath = "data.h5"
+
+        run(loggerpath)
+        exp_plot(loggerpath)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", "--with-ray", action="store_true")
+    args = parser.parse_args()
+    main(args)
