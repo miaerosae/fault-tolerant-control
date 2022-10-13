f3ef139b972f8b4f1e9c3c39b5452c46bd2381b1
Miae Kim <80808711+miaerosae@users.noreply.github.com>
2022-10-13 20:56:55 +0900

    add error of disturbance estimation, for faulted and non-faulted cases

--- ftc/result_for_Scenario2.py
+++ ftc/result_for_Scenario2.py
@@ -18,8 +18,9 @@ plt.rc("legend", fontsize=11)
 cfg = ftc.config.load()
 
 
-def exp_plot(path1):
-    data, info = fym.load(path1, with_info=True)
+def exp_plot(path1, path2):
+    data1, info = fym.load(path1, with_info=True)
+    data2 = fym.load(path2)
     rotor_min = info["rotor_min"]
     rotor_max = info["rotor_max"]
 
@@ -27,9 +28,9 @@ def exp_plot(path1):
     plt.figure(figsize=(6, 4.5))
 
     name = [r"$\lambda_1$", r"$\lambda_2$", r"$\lambda_3$", r"$\lambda_4$"]
-    for i in range(data["W"].shape[1]):
+    for i in range(data1["W"].shape[1]):
         plt.ylim([0-0.1, 1+0.1])
-        plt.plot(data["t"], data["W"][:, i, i], "--", label=name[i])
+        plt.plot(data1["t"], data1["W"][:, i, i], "--", label=name[i])
     plt.legend(loc=[0, 1.03], ncol=4, mode="expand")
     plt.xlabel("Time [sec]")
     plt.tight_layout()
@@ -40,17 +41,17 @@ def exp_plot(path1):
 
     rho = np.array([0.5, 0.25])
     rho_k = 0.5
-    pos_bounds = np.zeros((np.shape(data["x"]["pos"][:, 0, 0])[0]))
-    for i in range(np.shape(data["x"]["pos"][:, 0, 0])[0]):
-        pos_bounds[i] = (rho[0]-rho[1]) * np.exp(-rho_k*data["t"][i]) + rho[1]
+    pos_bounds = np.zeros((np.shape(data1["x"]["pos"][:, 0, 0])[0]))
+    for i in range(np.shape(data1["x"]["pos"][:, 0, 0])[0]):
+        pos_bounds[i] = (rho[0]-rho[1]) * np.exp(-rho_k*data1["t"][i]) + rho[1]
     ax = plt.subplot(311)
     for i, _label in enumerate([r"$e_x$", r"$e_y$", r"$e_z$"]):
         if i != 0:
             plt.subplot(311+i, sharex=ax)
-        plt.plot(data["t"], data["x"]["pos"][:, i, 0]-data["ref"][:, i, 0], "k-", label="Real Value")
-        plt.plot(data["t"], data["obs_pos"][:, i, 0], "b--", label="Estimated Value")
-        plt.plot(data["t"], pos_bounds, "r:", label="Prescribed Bound")
-        plt.plot(data["t"], -pos_bounds, "r:")
+        plt.plot(data1["t"], data1["x"]["pos"][:, i, 0]-data1["ref"][:, i, 0], "k-", label="Real Value")
+        plt.plot(data1["t"], data1["obs_pos"][:, i, 0], "b--", label="Estimated Value")
+        plt.plot(data1["t"], pos_bounds, "r:", label="Prescribed Bound")
+        plt.plot(data1["t"], -pos_bounds, "r:")
         plt.ylabel(_label)
         # plt.axvspan(5, 5.1, alpha=0.2, color="mediumslateblue")
         # plt.axvspan(7, 7.1, alpha=0.2, color="mediumslateblue")
@@ -72,16 +73,16 @@ def exp_plot(path1):
     bound = 45
     plt.ylim([-bound-5, bound+5])
 
-    angles = np.vstack([quat2angle(data["x"]["quat"][j, :, 0]) for j in range(len(data["x"]["quat"][:, 0, 0]))])
+    angles = np.vstack([quat2angle(data1["x"]["quat"][j, :, 0]) for j in range(len(data1["x"]["quat"][:, 0, 0]))])
     ax = plt.subplot(311)
     for i, _label in enumerate([r"$\phi$", r"$\theta$", r"$\psi$"]):
         if i != 0:
             plt.subplot(311+i, sharex=ax)
-        plt.plot(data["t"], np.rad2deg(angles[:, 2-i]), "k-", label="Real Value")
-        plt.plot(data["t"], np.rad2deg(data["obs_ang"][:, i, 0]), "b--", label="Estimated Value")
-        plt.plot(data["t"],
-                 np.ones((np.size(data["t"])))*bound, "r:", label="Prescribed Bound")
-        plt.plot(data["t"], -np.ones((np.size(data["t"])))*bound, "r:")
+        plt.plot(data1["t"], np.rad2deg(angles[:, 2-i]), "k-", label="Real Value")
+        plt.plot(data1["t"], np.rad2deg(data1["obs_ang"][:, i, 0]), "b--", label="Estimated Value")
+        plt.plot(data1["t"],
+                 np.ones((np.size(data1["t"])))*bound, "r:", label="Prescribed Bound")
+        plt.plot(data1["t"], -np.ones((np.size(data1["t"])))*bound, "r:")
         plt.ylabel(_label)
         if i == 0:
             plt.legend(loc=[0, 1.03], ncol=3, mode="expand")
@@ -108,16 +109,16 @@ def exp_plot(path1):
     for i, _label in enumerate(["p", "q", "r"]):
         if i != 0:
             plt.subplot(311+i, sharex=ax)
-        plt.plot(data["t"], np.rad2deg(data["x"]["omega"][:, i, 0]), "k-")
+        plt.plot(data1["t"], np.rad2deg(data1["x"]["omega"][:, i, 0]), "k-")
         if i == 2:
-            plt.plot(data["t"], np.ones((np.size(data["t"])))*bound_psi, "r:",
+            plt.plot(data1["t"], np.ones((np.size(data1["t"])))*bound_psi, "r:",
                      label="Prescribed Bound")
-            plt.plot(data["t"], -np.ones((np.size(data["t"])))*bound_psi, "r:")
+            plt.plot(data1["t"], -np.ones((np.size(data1["t"])))*bound_psi, "r:")
             plt.ylim([-bound_psi-15, bound_psi+15])
         else:
-            plt.plot(data["t"], np.ones((np.size(data["t"])))*bound, "r:",
+            plt.plot(data1["t"], np.ones((np.size(data1["t"])))*bound, "r:",
                      label="Prescribed Bound")
-            plt.plot(data["t"], -np.ones((np.size(data["t"])))*bound, "r:")
+            plt.plot(data1["t"], -np.ones((np.size(data1["t"])))*bound, "r:")
             plt.ylim([-bound-15, bound+15])
         plt.ylabel(_label)
         if i == 0:
@@ -141,12 +142,12 @@ def exp_plot(path1):
 
     name = [r"$\Omega_1$", r"$\Omega_2$", r"$\Omega_3$", r"$\Omega_4$"]
     ax = plt.subplot(411)
-    for i in range(data["rotors"].shape[1]):
+    for i in range(data1["rotors"].shape[1]):
         if i != 0:
             plt.subplot(411+i, sharex=ax)
         plt.ylim([rotor_min-5, np.sqrt(rotor_max)+5])
-        plt.plot(data["t"], np.sqrt(data["rotors"][:, i]), "k-", label="Response")
-        plt.plot(data["t"], np.sqrt(data["rotors_cmd"][:, i]), "r--", label="Command")
+        plt.plot(data1["t"], np.sqrt(data1["rotors"][:, i]), "k-", label="Response")
+        plt.plot(data1["t"], np.sqrt(data1["rotors_cmd"][:, i]), "r--", label="Command")
         plt.ylabel(name[i])
         if i == 0:
             plt.legend(loc="lower right", bbox_to_anchor=[1, 1.03], ncol=2)
@@ -171,7 +172,7 @@ def exp_plot(path1):
     for i, _label in enumerate([r"$u_{1}$", r"$u_{2}$", r"$u_{3}$", r"$u_{4}$"]):
         if i != 0:
             plt.subplot(411+i, sharex=ax)
-        plt.plot(data["t"], data["virtual_u"][:, i], "k-", label=_label)
+        plt.plot(data1["t"], data1["virtual_u"][:, i], "k-", label=_label)
         if i == 0:
             plt.ylabel(_label, labelpad=23)
         elif i == 1:
@@ -197,27 +198,34 @@ def exp_plot(path1):
     # disturbance
     plt.figure(figsize=(12, 9))
 
-    real_dist = np.zeros((6, np.size(data["t"])))
-    ext_dist = cfg.simul_condi.ext_unc
-    for i in range(np.size(data["t"])):
-        t = data["t"][i]
-        real_dist[:, i] = get_sumOfDist(t, ext_dist).ravel()
+    real_dist = np.zeros((6, np.size(data1["t"])))
+    real_dist2 = np.zeros((6, np.size(data2["t"])))
+    for i in range(np.size(data1["t"])):
+        t = data1["t"][i]
+        real_dist[:, i] = get_sumOfDist(t, True).ravel()
+        real_dist2[:, i] = get_sumOfDist(t, True).ravel()
     for i in range(3):
         real_dist[i, :] = (real_dist[i, :]
-                           + data["model_uncert_vel"][:, i, 0]
-                           + data["int_uncert_vel"][:, i, 0])
+                           + data1["model_uncert_vel"][:, i, 0]
+                           + data1["int_uncert_vel"][:, i, 0])
+        real_dist2[i, :] = (real_dist2[i, :]
+                            + data2["model_uncert_vel"][:, i, 0]
+                            + data2["int_uncert_vel"][:, i, 0])
     for i in range(3):
         real_dist[i+3, :] = (real_dist[i+3, :]
-                             + data["f"][:, i, 0]
-                             + data["model_uncert_omega"][:, i, 0])
+                             + data1["f"][:, i, 0]
+                             + data1["model_uncert_omega"][:, i, 0])
+        real_dist2[i+3, :] = (real_dist2[i+3, :]
+                              + data2["f"][:, i, 0]
+                              + data2["model_uncert_omega"][:, i, 0])
 
     ax = plt.subplot(611)
     for i, _label in enumerate([r"$e_{3x}$", r"$e_{3y}$", r"$e_{3z}$",
                                 r"$e_{3\phi}$", r"$e_{3\theta}$", r"$e_{3\psi}$"]):
         if i != 0:
             plt.subplot(611+i, sharex=ax)
-        plt.plot(data["t"], real_dist[i, :], "k-", label="Real Value")
-        plt.plot(data["t"], data["dist"][:, i, 0], "b--", label="Estimated Value")
+        plt.plot(data1["t"], real_dist[i, :], "k-", label="Real Value")
+        plt.plot(data1["t"], data1["dist"][:, i, 0], "b--", label="Estimated Value")
         plt.ylabel(_label)
         if i == 0:
             plt.legend(loc="lower right", bbox_to_anchor=[1, 1.03], ncol=2)
@@ -231,6 +239,21 @@ def exp_plot(path1):
         # plt.axvline(10, alpha=0.8, color="mediumslateblue", linewidth=0.5)
         # plt.axvline(14, alpha=0.8, color="mediumslateblue", linewidth=0.5)
         # plt.axvline(15, alpha=0.8, color="mediumslateblue", linewidth=0.5)
+    plt.gcf().supxlabel("Time [sec]")
+    plt.tight_layout()
+
+    # disturbance err
+    plt.figure(figsize=(12, 9))
+    ax = plt.subplot(611)
+    for i, _label in enumerate([r"$\hat{e}_{3x}$", r"$\hat{e}_{3y}$", r"$\hat{e}_{3z}$",
+                                r"$\hat{e}_{3\phi}$", r"$\hat{e}_{3\theta}$", r"$\hat{e}_{3\psi}$"]):
+        if i != 0:
+            plt.subplot(611+i, sharex=ax)
+        plt.plot(data1["t"], data1["dist"][:, i, 0] - real_dist[i, :], "k-", label="With Faults")
+        plt.plot(data1["t"], data2["dist"][:, i, 0] - real_dist2[i, :], "g--", label="Without Faults")
+        plt.ylabel(_label)
+        if i == 0:
+            plt.legend(loc="lower right", bbox_to_anchor=[1, 1.03], ncol=2)
     plt.gcf().supxlabel("Time [sec]")
     plt.tight_layout()
 
@@ -253,15 +276,15 @@ def exp_plot(path1):
         if i != 0:
             plt.subplot(331+i, sharex=ax)
         if i == 1:
-            plt.plot(data["t"], data["gain"][:, i, 0], "r", label="Real Gain")
+            plt.plot(data1["t"], data1["gain"][:, i, 0], "r", label="Real Gain")
         else:
-            plt.plot(data["t"], data["gain"][:, i, 0], "r")
+            plt.plot(data1["t"], data1["gain"][:, i, 0], "r")
         if i % 3 == 0:
-            plt.plot(data["t"], np.ones(np.shape(data["t"]))*kP1, "b--", label="PID-like Gain")
+            plt.plot(data1["t"], np.ones(np.shape(data1["t"]))*kP1, "b--", label="PID-like Gain")
         elif i % 3 == 1:
-            plt.plot(data["t"], np.ones(np.shape(data["t"]))*kD1, "b--")
+            plt.plot(data1["t"], np.ones(np.shape(data1["t"]))*kD1, "b--")
         elif i % 3 == 2:
-            plt.plot(data["t"], np.ones(np.shape(data["t"]))*kI1, "b--", label="PID-like Gain")
+            plt.plot(data1["t"], np.ones(np.shape(data1["t"]))*kI1, "b--", label="PID-like Gain")
         if i == 0:
             plt.ylabel("x subsystem", labelpad=10)
             plt.title(r"$k_{P}$")
@@ -296,15 +319,15 @@ def exp_plot(path1):
         if i != 0:
             plt.subplot(331+i, sharex=ax)
         if i == 1:
-            plt.plot(data["t"], data["gain"][:, i+9, 0], "r", label="Real Gain")
+            plt.plot(data1["t"], data1["gain"][:, i+9, 0], "r", label="Real Gain")
         else:
-            plt.plot(data["t"], data["gain"][:, i+9, 0], "r")
+            plt.plot(data1["t"], data1["gain"][:, i+9, 0], "r")
         if i % 3 == 0:
-            plt.plot(data["t"], np.ones(np.shape(data["t"]))*kP2, "b--", label="PID-like Gain")
+            plt.plot(data1["t"], np.ones(np.shape(data1["t"]))*kP2, "b--", label="PID-like Gain")
         elif i % 3 == 1:
-            plt.plot(data["t"], np.ones(np.shape(data["t"]))*kD2, "b--")
+            plt.plot(data1["t"], np.ones(np.shape(data1["t"]))*kD2, "b--")
         elif i % 3 == 2:
-            plt.plot(data["t"], np.ones(np.shape(data["t"]))*kI2, "b--", label="PID-like Gain")
+            plt.plot(data1["t"], np.ones(np.shape(data1["t"]))*kI2, "b--", label="PID-like Gain")
         if i == 0:
             plt.ylabel(r"$\phi$" + " subsystem", labelpad=10)
             plt.title(r"$k_{P}$")
@@ -335,4 +358,4 @@ def exp_plot(path1):
 
 
 if __name__ == "__main__":
-    exp_plot("Scenario2.h5")
+    exp_plot("Scenario2.h5", "Scenario2_nofault.h5")
